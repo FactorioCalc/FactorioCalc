e5b0c1466eb9fc847ac95186e34dc35aef3f5459
Kevin Atkinson <k@kevina.org>
2023-01-29 17:53:59 -0500

    Fix Tests.

--- factoriocalc/__init__.py
+++ factoriocalc/__init__.py
@@ -3,21 +3,26 @@ __all__ = ['itm', 'rcp', 'rcpinst', 'config']
 from .fracs import frac, div, Frac
 __all__ += ['frac', 'div', 'Frac']
 
+from . import core
 from .core import *
 __all__ += core.__all__
 
+from . import machines
 from .machines import *
 __all__ += machines.__all__
 
+from . import units
 from .units import *
 __all__ += units.__all__
 
+from . import data
 from .data import *
 __all__ += data.__all__
 
 from . import _import_raw
 _import_raw.doit()
 
+from . import presets
 from .presets import *
 __all__ += presets.__all__
 
@@ -25,12 +30,15 @@ from . import box as _box
 from .box import *
 __all__ += _box.__all__
 
+from . import solver
 from .solver import SolveRes
 __all__ += ['SolveRes']
 
+from . import blueprint
 from .blueprint import *
 __all__ += blueprint.__all__
 
+from . import jsonconv
 from .jsonconv import *
 __all__ += jsonconv.__all__
 
@@ -38,6 +46,7 @@ from . import produce as _produce
 from .produce import *
 __all__ += _produce.__all__
 
+from . import helper
 from .helper import *
 __all__ += helper.__all__
 
